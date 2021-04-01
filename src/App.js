a4d395a349c4bc3d302ba1931a369ae482e134e4
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2021-04-01 18:53:59 +0530

    Init Changes

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./assets/main.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="bg-blue-800">
+        <div className="text-center text-white font-bold text-4xl">
+          Welcome to Food Blog
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
