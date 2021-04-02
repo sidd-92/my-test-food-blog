f00c43d87c984911a9b24cc7b2ea526b719165da
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2021-04-02 11:18:33 +0530

    Setup Tailwind

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import "./assets/main.css";
-
 class App extends React.Component {
   render() {
     return (
-      <div className="bg-blue-800">
-        <div className="text-center text-white font-bold text-4xl">
+      <div className="bg-blue-300">
+        <div className="text-center text-gray-900 font-bold text-4xl">
           Welcome to Food Blog
         </div>
       </div>
